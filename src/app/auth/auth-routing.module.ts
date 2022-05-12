1737e17cb2fc251b903684071b2356e292b5fdb4
Carlos-nicolas <carlosalexis52@hotmail.com>
2022-05-12 11:38:15 -0500

    add routing in auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path:'**',
+    redirectTo: 'dashboard',
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
